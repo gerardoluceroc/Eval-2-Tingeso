b43eae48ac02d554c673634beaf98f7600b9dec2
Gerardo Lucero <gerardo.lucero.c@usach.cl>
2022-11-22 21:15:07 -0300

    Se trabaja en el frontend con react
    hubo tambien unos pequeños arreglos en los microservicios

--- inasistencia-service/src/main/java/com/service/inasistenciaservice/controllers/InasitenciaController.java
+++ inasistencia-service/src/main/java/com/service/inasistenciaservice/controllers/InasitenciaController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import com.service.inasistenciaservice.services.JwtUtilService;
 
 
 @Controller
+@CrossOrigin(origins="http://localhost:3001")
 @RequestMapping()
 public class InasitenciaController {
     @Autowired
